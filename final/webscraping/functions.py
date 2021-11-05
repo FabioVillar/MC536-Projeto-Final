b3c3088b3d967a199d34883389184718958df3e8
José Augusto <j200025@dac.unicamp.br>
2021-11-05 11:29:40 -0300

    Class assignment update (WorldCup, Teams, Players)

--- final/webscraping/functions.py
+++ final/webscraping/functions.py
@@ -2,46 +2,65 @@ from bs4 import BeautifulSoup
 import requests
 from classes import *
 
+def create_new_player(name, stats):
+    new_player = Player()
+    new_player.name = name
+    new_player.position = stats[0].text
+    new_player.age = stats[1].text
+    new_player.goals_scored = stats[6].text
+    new_player.assists = stats[7].text
+    new_player.yellow_cards = stats[11].text
+    new_player.red_cards = stats[12].text
 
-def create_new_team(link, name):
+    return new_player
 
-    new_team = Team()
-    new_team.name = name
+def create_new_team(link, name, page_id):
+
+    #requests
     team_page = requests.get('https://fbref.com/'+link).text #request
     soup = BeautifulSoup(team_page, 'lxml') #parsing
     content = soup.find('div', id='content')
-    table = content.find('div', class_='table_container current is_setup')
-    print(table)
+    table = content.find('div', id=f'div_stats_standard_{page_id}')
     player_links = table.find('tbody')
     players = player_links.find_all('tr')
+
+    
+    new_team = Team()
+    new_team.name = name
+
     for i in players:
         stats = i.find_all('td')
-        new_player = Player(i.find('th')['csk'], stats[0].text, stats[1].text, stats[6].text, stats[7].text, stats[11].text, stats[12].text)
-        Team.players.append(new_player)
+        new_player = create_new_player(i.find('th').text, stats)
+        new_team.players.append(new_player)
     return new_team
 
 def create_new_cup(year, page_id):
-    link = None
+
+    link = 'https://fbref.com/en/comps/106'
     if (year == 2019):
-
-        link = 'https://fbref.com/en/comps/106/qual/Womens-World-Cup-Qualifying-Rounds'
+        link = link + '/qual/Womens-World-Cup-Qualifying-Rounds'
     else:
+        link = link + f'/{page_id}/qual/{year}-Womens-World-Cup-Qualifying-Rounds'
 
-        link = f'https://fbref.com/en/comps/106/{page_id}/qual/{year}-Womens-World-Cup-Qualifying-Rounds'
-    
-    new_cup = WorldCup()
-    new_cup.year = year
+    #requests
+    cup_page = requests.get(link).text
+    soup = BeautifulSoup(cup_page, 'lxml')
 
-    cup_page = requests.get(link).text #request
-    soup = BeautifulSoup(cup_page, 'lxml') #parsing
     table = soup.find('div', id='div_qualification')
     team_links = table.find('tbody')
     teams = team_links.find_all('tr')
 
+    #creating
+    new_cup = WorldCup()
+    new_cup.year = year
+    new_cup.host = soup.find('div', id='meta').find_all('p')[0].find('a').text
+
+    print(new_cup.host)
     team_count = 0
     for i in teams:
-        new_team = create_new_team(i.find('a')['href'], i.find('a').text)
-        WorldCup.teams.append(new_team)
+        new_team = create_new_team(i.find('a')['href'], i.find('a').text, page_id)
+        new_cup.teams.append(new_team)
+        print(i.find('a').text)
         team_count+=1
 
     new_cup.number_of_teams = team_count
--- final/webscraping/index.py
+++ final/webscraping/index.py
@@ -6,11 +6,21 @@ if __name__ == "__main__":
     start_year = 1991
     final_year = 2019
     page_id = 1779
-    cup_list = [0]*8
+    cup_list = []
 
-    for i in cup_list:
+
+    while (start_year<=final_year):
+        print()
+        print(start_year)
         i = create_new_cup(start_year, page_id)
+        print()
+        for j in i.teams:
+            print(j.name)
+            print()
+            print("Players:")
+            print()
+            for k in j.players:
+                print(k.name, k.age, k.position, k.goals_scored)
+            print()
         start_year += 4
         page_id += 1
-        print("Copa do ano de " + str(i.year))
-        print(i.teams)
