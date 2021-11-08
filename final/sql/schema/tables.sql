f0b4e7d0c9d3c05c02f80a6ac9ed40c4046ebec3
Arthurcbaia <arthurceminbaia@gmail.com>
2021-11-08 16:44:13 -0300

    sql fixes

--- final/sql/schema/tables.sql
+++ final/sql/schema/tables.sql
@@ -14,93 +14,85 @@ DROP TABLE if exists WorldCup;
 
 -- WC -> Group -> team -> player -> match -> events -> awards 
 CREATE TABLE if not exists WorldCup (
-    Year_wc INTEGER not null,
-    Host VARCHAR(50) not null,
-    NumberOfParticipants INTEGER not null,
-    Winner VARCHAR(50) not null,
+    year_wc INTEGER not null,
+    host VARCHAR(50) not null,
+    winner VARCHAR(50) not null,
     PRIMARY KEY(Year_wc)
 );
 
-CREATE TABLE if not exists Group_wc (
-    Group_id VARCHAR(50) not null,
-    NumberOfTeams INTEGER not null,
-    Year_wc INTEGER not null,
-    PRIMARY Key(Group_id),
-    FOREIGN KEY(Year_wc) REFERENCES WorldCup(Year_wc)
-);
 
 CREATE TABLE if not exists Team_wc(
-    Team_id integer NOT NULL AUTO_INCREMENT,
-    Team_name VARCHAR(50) not null,
-    NumberOfPlayers INTEGER not null,
-    Coach VARCHAR(50) not null,
-    Group_in_wc VARCHAR(50) not null,
-    Group_points INTEGER not null,
-    Ranking_group INTEGER not null,
-    GoalsScored INTEGER not null,
-    GoalsSuffered INTEGER not null,
-    Wins INTEGER not null,
-    Draws INTEGER not null,
-    Looses INTEGER not null,
+    team_id integer NOT NULL AUTO_INCREMENT,
+    year_wc integer not null,
+    team_name VARCHAR(50) not null,
+    coach VARCHAR(50) not null,
+    group_in_wc VARCHAR(50) not null,
+    group_points INTEGER not null,
+    ranking_group INTEGER not null,
+    goals_scored INTEGER not null,
+    goals_suffered INTEGER not null,
+    wins INTEGER not null,
+    draws INTEGER not null,
+    losses INTEGER not null,
     PRIMARY key(team_id),
-    FOREIGN KEY(Group_in_wc) REFERENCES Group_wc(Group_id)
+    FOREIGN key(year_wc) REFERENCES WorldCup(year_wc)
 );
 
 CREATE TABLE Player_wc (
-    Player_id integer NOT NULL AUTO_INCREMENT,
-    Team_name VARCHAR(50) not null,
-    Team_id integer not null,
-    Player_name VARCHAR(50) not null,
-    Player_number INTEGER not null,
-    Age INTEGER not null,
-    Position VARCHAR(50) not null,
-    Goals INTEGER not null,
-    YellowCards INTEGER not null,
-    RedCards INTEGER not null,
+    player_id integer NOT NULL AUTO_INCREMENT,
+    team_name VARCHAR(50) not null,
+    team_id integer not null,
+    player_name VARCHAR(50) not null,
+    player_number INTEGER not null,
+    age INTEGER not null,
+    position VARCHAR(50) not null,
+    goals INTEGER not null,
+    yellow_cards INTEGER not null,
+    red_cards INTEGER not null,
     PRIMARY key(player_id),
-    FOREIGN KEY(Team_id) REFERENCES Team_wc(Team_id)
+    FOREIGN KEY(team_id) REFERENCES Team_wc(team_id)
 );
 
 CREATE TABLE Match_wc (
-    Match_id INTEGER not null AUTO_INCREMENT,
-    Penaltys VARCHAR(50),
-    Phase VARCHAR(50) not null,
-    TeamA integer NOT null,
-    TeamB integer not null,
-    Score VARCHAR(50) not null,
-    Stadium VARCHAR(50),
-    Attendance INTEGER,
-    Referee VARCHAR(50),
-    FormationA VARCHAR(50),
-    FormationB VARCHAR(50),
-    LineupA TEXT,
-    LineupB TEXT,
-    ReservesA TEXT,
-    ReservesB TEXT,
-    Possesion VARCHAR(50),
-    PRIMARY key(Match_id),
-    FOREIGN KEY(TeamA) REFERENCES Team_wc(Team_id),
-    FOREIGN KEY(TeamB) REFERENCES Team_wc(Team_id)
+    match_id INTEGER not null AUTO_INCREMENT,
+    penaltys VARCHAR(50),
+    phase VARCHAR(50) not null,
+    teamA integer NOT null,
+    teamB integer not null,
+    score VARCHAR(50) not null,
+    stadium VARCHAR(50),
+    attendance INTEGER,
+    referee VARCHAR(50),
+    formation_A VARCHAR(50),
+    formation_B VARCHAR(50),
+    lineupA TEXT,
+    lineupB TEXT,
+    reservesA TEXT,
+    reservesB TEXT,
+    possesion VARCHAR(50),
+    PRIMARY key(match_id),
+    FOREIGN KEY(teamA) REFERENCES Team_wc(team_id),
+    FOREIGN KEY(teamB) REFERENCES Team_wc(team_id)
 );
 
 CREATE TABLE Events_wc (
-    Event_id integer not null,
-    Match_id INTEGER not null,
-    Event_desc TEXT not null,
-    Match_time float,
-    Team integer not null,
-    Player integer not null,
-    PRIMARY key(Event_id),
-    FOREIGN KEY(Player) REFERENCES Player_wc(player_id),
-    FOREIGN KEY(Team) REFERENCES Team_wc(Team_id),
-    FOREIGN Key(Match_Id) REFERENCES Match_wc(Match_id)
+    event_id integer not null,
+    match_id INTEGER not null,
+    event_desc TEXT not null,
+    match_time float,
+    team integer not null,
+    player integer not null,
+    PRIMARY key(event_id),
+    FOREIGN KEY(player) REFERENCES Player_wc(player_id),
+    FOREIGN KEY(team) REFERENCES Team_wc(team_id),
+    FOREIGN Key(match_Id) REFERENCES Match_wc(match_id)
 );
 
 CREATE TABLE Awards_wc (
     award_type TEXT,
-    Year_wc INTEGER not null,
-    Player integer not null,
-    Team integer not null,
-    FOREIGN KEY(Year_wc) REFERENCES WorldCup(Year_wc),
-    FOREIGN Key(Player) REFERENCES Player_wc(Player_id)
+    year_wc INTEGER not null,
+    player integer not null,
+    team integer not null,
+    FOREIGN KEY(year_wc) REFERENCES WorldCup(year_wc),
+    FOREIGN Key(player) REFERENCES Player_wc(player_id)
 );
